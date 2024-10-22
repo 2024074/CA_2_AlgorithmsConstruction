341bbbf27ec16ddc50978bd492e560a5be5158d7
2024074 <2024074@student.cct.ie>
2024-10-22 16:44:00 +0100

    test
    test

---
+++ CA_2/TechCompany/src/CA_2/TechCompany_Menu.java
@@ -0,0 +1,10 @@
+
+package CA_2;
+import java.util.Scanner;
+public class TechCompany_Menu {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
